569e97597e156007a2a1fb83bb8ab3e9524370d1
lowqwroby <eltiobvs@gmail.com>
2022-12-14 17:52:48 +0300

    Добавьте файлы проекта.

---
+++ Task_7_1_PM01_N3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Variant10
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Введите строку: ");
+                String s = new String(Console.ReadLine());
+                Console.Write("Введите первую подстроку: ");
+                String substr1 = new String(Console.ReadLine());
+                Console.Write("Введите вторую подстроку: ");
+                String substr2 = new String(Console.ReadLine());
+                Console.WriteLine("Исходная строка: " + s);
+                String news = s.Replace(substr1, substr2);
+                if(s == news)
+                {
+                    Console.WriteLine("Строка не была изменена");
+                    return;
+                }
+                Console.WriteLine("Измененная строка: " + news);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверно введены значения");
+            }
+            catch
+            {
+                Console.WriteLine("Что-то пошло не так");
+            }
+            
+
+        }
+    }
+}
---
+++ Task_7_2_PM01_N3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Variant10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите предложение: ");
+            String s = new string(Console.ReadLine());
+            string[] str = s.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder max = new StringBuilder("");
+            int index = max.Length;
+            for(int i = 0; i<str.Length; i++)
+            {
+                if(str[i].Length >= index)
+                {
+                    if (str[i].Length == index)
+                    {
+                        max.Append(" " + str[i]);
+                        continue;
+                    }
+                    max.Clear();
+                    max.Append(str[i]);
+                    index = max.Length;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в предложении - " + max);
+        }
+    }
+}
